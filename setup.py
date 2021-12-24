d8fbd22dfcb8188b822de1d4b567826d8ff0808d
Wllew4 <willjanelle2@gmail.com>
2021-12-24 01:23:36 -0500

    fixed readme

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
 	name='bofa_scraper',
-	version='0.1.1',
+	version='0.1.2',
 	description='Simple Python web-scraper to get personal transaction data from BofA account.',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
 	url='https://github.com/Wllew4/bofa_scraper',
 	author='Wllew4',
 	license='GPLv3',
